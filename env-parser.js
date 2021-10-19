fb711d7b77d11ee2bf561cf82acf4e26a979a918
obscuredc <77344064+obscuredc@users.noreply.github.com>
2021-10-18 20:50:11 -0400

    Update env-parser.js

--- env-parser.js
+++ env-parser.js
@@ -1,5 +1,19 @@
-/** 
-Goal:
-FOO=BAR 
-outputs {FOO: "BAR"}
-**/
+function envparser(t) {
+  var r = {};
+  //t expected to be string
+  //we parse the format:
+  //<VARNAME>=<VALUE> and output object
+  //each entry is seperated by \n.
+  var Entries = t.split("\n");
+  var i;
+  for(i = 0; i < Entries.length; i++) {
+    //some magic lmao
+    var Name = Entries[i].slice(0, Entries[i].indexOf("="));
+    //console.log(`NAME=${Name}`);
+    var Value = Entries[i].slice(Entries[i].indexOf("=")+1, Entries[i].length);
+    //console.log(`VALUE=${Value}`);
+    //Now set r.
+    r[Name] = Value;
+  }
+  return r;
+}
