d7d89d9771cf9f0e6ba5d2970fffda2e9b3919b6
obscuredc <77344064+obscuredc@users.noreply.github.com>
2021-10-20 07:36:54 -0400

    Update terminal-emu.js

--- terminal-emu.js
+++ terminal-emu.js
@@ -1,12 +1,50 @@
+//warning: untested & unready!
+
 //goal
 // pkg install -f "core"
 //[Keyword:"pkg",Keyword:"install",Flag:"f",Data:"core"]
 //[System:pkg, SubCommand:install, Flags:["f"], Parameters: ["core"]
+/** global helpers */
+const HString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_/";
+const HFlag = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
+const HKeyword = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 class Lexer {
   constructor(Text) {
     this.Text = Text;
-    this.Output;
+    this.Index = 0;
+    this.Tokens = [];
+
+    /** types */
+    this.BuildString = false;
+    this.BuildFlag = false;
+    this.BuildKeyword = false;
+
+    this.Builder = "";
+  }
+  Lex() {
+    console.log(`beginning lex`);
+    while(this.Index < this.Text.length) {
+      var ctok = this.Text[this.Index];
+      if(HKeyword.includes(ctok) == true && this.BuildString == false && this.BuildFlag == false) {
+        //ok we can start keyword
+        this.BuildKeyword = true;
+      } else if (ctok == "\"") {
+        this.BuildString = !this.BuildString;
+      } else if (ctok == "-" && this.BuildString == false && this.BuildFlag == false) {
+        this.BuildFlag = true;
+      }
+      //see where characters fit
+      if(this.BuildString == true && HString.includes(ctok) == true) {
+        this.Builder += ctok;
+      } else if (HString.includes(ctok) == false) {
+        //throw error: Unknown Character in a string position == index
+      }
+
+      console.log(`Lex index=${this.Index}, char=${this.Text[this.Index]}`);
+      this.Index++;
+    }
+    console.log(`ended lex`);
   }
   ThrowLog(Message) {
     console.log(Message);
@@ -17,7 +55,6 @@ class Lexer {
   ThrowError() {
     console.error(Message);
   }
-  Lex() {
-    //main function for lexing
-  }
 }
+var lexer = new Lexer(`help -cmd "help"`); //=> [Keyword: "help", Flag: "cmd", String: "help"]
+lexer.Lex();
